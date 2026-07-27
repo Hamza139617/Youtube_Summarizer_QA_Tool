8d2d10e7844022bc3cfeb1ba040bd2cfe307b30d
Hamza <hamza139617@gmail.com>
2026-07-27 21:47:26 +0500

    completed the main function completely

---
+++ main.py
@@ -0,0 +1,14 @@
+from interface import create_interface
+
+
+def main():
+    """
+    The main summarizer and qa bot engine
+    """
+
+    create_interface()
+
+
+
+if __name__ == "__main__":
+    main()
