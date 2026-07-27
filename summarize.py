9e2acb33186d742f65755c1af79a75802519ebf9
Hamza <hamza139617@gmail.com>
2026-07-27 17:00:56 +0500

    completed the summary creation part now working on the qa bot

--- summarize.py
+++ summarize.py
@@ -1,5 +1,8 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from transcript import get_transcript, process
+from ai_model import creating_llm
+
 
 def create_summary_prompt():
     """
@@ -35,5 +38,80 @@ def create_summary_chain(llm, prompt):
 
     return chain
 
+
+def retrieving_summary(query, faiss_index, k=7):
+    """
+    Retrieve relevant context from the FAISS index based on the user's query
+    """
+
+    relevant_context = faiss_index.similarity_search(query, k=k)
+
+    return relevant_context
+
+
+def create_qa_prompt_template():
+    """
+    for creating the prompt template for question and answering based on video content
+    """
+
+    qa_template = """
+You are an expert assistant providing detailed answers based on the following video content.
+
+    Relevant Video Context: {context}
+
+    Based on the above context, please answer the following question:
+    Question: {question}
+"""
+
+    prompt = PromptTemplate(
+        template=qa_template,
+        input_variables=["context", "question"]
+    )
+
+    return prompt
+
+
+
+def generate_answer(question, faiss_index, qa_chain, k=7):
+    """
+    Retrieve relevant context and generate an answer based on user input
+    """
+
+    relevant_context = retrieving_summary(question, faiss_index, k=k)
+
+    answer = qa_chain.predict(context=relevant_context, question=question)
+
+    return answer
+
+
+def summarize_video(video_url: str):
+    """
+    For summarizing the video
+    """
+
+    global fetched_transcript, processed_transcript
+
+    if video_url:
+        fetched_transcript = get_transcript(video_url)
+        processed_transcript = process(fetched_transcript)
+    else:
+        return "Please provide a valid Youtube URL."
+
+    if processed_transcript:
+
+        llm = creating_llm()
+
+        summary_prompt = create_summary_prompt()
+
+        summary_chain = create_summary_chain(llm, summary_prompt)
+
+        summary = summary_chain.invoke({"transcript":processed_transcript})
+
+        return summary
+
+    else:
+
+        return "No transcript available. Please fetch the transcript first"
+        
 
 
